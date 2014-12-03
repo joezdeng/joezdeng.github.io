2b259f87e99539a372aa3e5cd50de3ecbea49317
joezdeng <joe.z.deng@gmail.com>
2014-12-02 23:53:29 -0600

    hamburger menu

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	});
 
 
-	$('nav ul a').click( function(e){
+	$('nav a').click( function(e){
 		e.preventDefault();
 
 		var link = $(this).attr('href');
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		var height = $(link).position().top;
 		console.log(height);
 
-		var time = (height/3)
+		var time = (height/4)
 
 		$('html,body').animate({
 			scrollTop: height
@@ -66,5 +66,40 @@ $(document).ready(function(){
 		else(inClick) = false;
 
 	});
+
+
+    $('#menuButton').click(function () {
+        $('#menuList').toggleClass('hidden');
+
+        $('#menuButton').toggleClass('buttonInactive');
+
+    });
+
+    $(document).scroll(function() {
+    	$('#menuList').addClass('hidden');
+
+    	$('#menuButton').addClass('buttonInactive');
+
+    });
+
+
+    if (window.matchMedia('(max-width:960px)')) {
+    	var buttonTop = $('#menuButton').offset().top;
+
+    	$(window).scroll(function() {
+    		if ($(window).scrollTop() > buttonTop) {
+    			$('#menuButton').css({position: "fixed", top: "0"});
+
+    			$('#menuList').css({position: "fixed", top: "3.5em"});
+    		
+    		} else {
+    			console.log ('working');
+    			$('#menuButton').css({position: "absolute", top: "14.6em"});
+
+    			$('#menuList').css({position: "absolute", top: "18.1em"});
+
+    		}
+    	});
+    }
 
 });
